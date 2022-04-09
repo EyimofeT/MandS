4e8fd712b189f6ce593bd29b5e85ffb7acd905d5
Mofii <mofii4u@gmail.com>
2022-04-09 15:32:55 +0100

    made working prototype

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 150)
 data.interpolate(method ='linear', limit_direction ='forward')
-# data.interpolate()
+pd.DataFrame(data).fillna(data.mean(), inplace=True)
+data.interpolate()
 #pd.reset_option('display.max_rows|display.max_columns|display.width')
 
-#print(data)
-print(data.head())
+print(data)
+#print(data.head())
 # data.head()
 # print('here now : \t')
 # print(data.tail())
@@ -49,13 +50,13 @@ train=data.drop([
 'Last Communication Contact from Initial Pathologic Diagnosis Date',
 'Birth from Initial Pathologic Diagnosis Date',
 'Last Alive Less Initial Pathologic Diagnosis Date Calculated Day Value',
-# 'Disease Free (Months)',
-# 'Disease Free Status',
-# 'Months of disease-specific survival',
+ 'Disease Free (Months)',
+ 'Disease Free Status',
+ #'Months of disease-specific survival',
 'Disease-specific Survival status',
 'Ethnicity Category',
 'Form completion date',
-'Fraction Genome Altered',
+#'Fraction Genome Altered',
 'Neoplasm Histologic Grade',
 'Neoadjuvant Therapy Type Administered Prior To Resection Text',
 'ICD-10 Classification',
@@ -63,39 +64,40 @@ train=data.drop([
 'International Classification of Diseases for Oncology, Third Edition ICD-O-3 Site Code',
 'Informed consent verified',
 'In PanCan Pathway Analysis',
-'MSI MANTIS Score',
-'MSIsensor Score',
-'Mutation Count',
+#'MSI MANTIS Score',
+#'MSIsensor Score',
+#'Mutation Count',
 'New Neoplasm Event Post Initial Therapy Indicator',
 'Oncotree Code',	
-'Overall Survival (Months)',
+#'Overall Survival (Months)',
 'Overall Survival Status',
 'Other Patient ID',
 'American Joint Committee on Cancer Metastasis Stage Code',
 'Neoplasm Disease Lymph Node Stage American Joint Committee on Cancer Code',
 'American Joint Committee on Cancer Tumor Stage Code',
 'Person Neoplasm Cancer Status',
-'Progress Free Survival (Months)',	
+#'Progress Free Survival (Months)',	
 'Progression Free Status',	
 'Primary Lymph Node Presentation Assessment',
 'Prior Diagnosis',
 'Race Category',
 'Radiation Therapy',
-'Ragnum Hypoxia Score',
+#'Ragnum Hypoxia Score',
 'Number of Samples Per Patient',
 'Sample Type',
-'Sex',
+#'Sex',
 'Somatic Status',
 'Subtype',
 'Tissue Prospective Collection Indicator',
 'Tissue Retrospective Collection Indicator',
 'Tissue Source Site',
 'Tissue Source Site Code',
-'TMB (nonsynonymous)',	
+#'TMB (nonsynonymous)',	
 'Tumor Disease Anatomic Site',
-'Tumor Type',
-'Patient Weight',
-'Winter Hypoxia Score'],axis=1)
+'Tumor Type'
+#'Patient Weight',
+#'Winter Hypoxia Score'
+ ],axis=1)
 
 test=data['Overall Survival Status']
 
@@ -103,9 +105,9 @@ test=data['Overall Survival Status']
 # train= train.apply(pd.to_numeric, errors='coerce')
 # test = test.apply(pd.to_numeric, errors='coerce')
 # train.fillna(0, inplace=True)
-# test.fillna(0, inplace=True)
-train.fillna(data.mean(), inplace=True)
-test.fillna(data.mean(), inplace=True)
+#  test.fillna(0, inplace=True)
+# train.fillna(data.mean(), inplace=True)
+# test.fillna(data.mean(), inplace=True)
 
 X_train,X_test,y_train,y_test=train_test_split(train,test,test_size=0.3,random_state=2)
 
@@ -113,8 +115,8 @@ regr = LinearRegression()
 
 regr.fit(X_train,y_train)
 pred = regr.predict(X_test)
-# print(pred)
-# print("accurace : ")
-# print(regr.score(X_test,y_test))
+print(pred)
+print("accuracy : ")
+print(regr.score(X_test,y_test))
 #git config --global --edit::
 
