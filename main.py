42b35e7300ed5cf131241676658606d985f6ff38
Mofii <mofii4u@gmail.com>
2022-04-09 12:25:10 +0100

    Update to the work

--- main.py
+++ main.py
@@ -25,12 +25,24 @@ data = pd.read_csv('datafile.tsv', sep='\t')
 #plt.show()
 
 
-train=data.drop(['American Joint Committee on Cancer Metastasis Stage Code','American Joint Committee on Cancer Publication Version Type','Subtype','Sample Type','Patient ID','Overall Survival Status','Number of Samples Per Patient'],axis=1)
+train=data.drop(['Overall Survival Status','American Joint Committee on Cancer Metastasis Stage Code','American Joint Committee on Cancer Publication Version Type','Subtype','Sample Type','Patient ID','Number of Samples Per Patient'],axis=1)
 
 test=data['Overall Survival Status']
 
-X_train,X_test,Y_train,Y_test=train_test_split(train,test,test_size=0.3,random_state=2)
+
+train= train.apply(pd.to_numeric, errors='coerce')
+test = test.apply(pd.to_numeric, errors='coerce')
+train.fillna(0, inplace=True)
+test.fillna(0, inplace=True)
+
+X_train,X_test,y_train,y_test=train_test_split(train,test,test_size=200,random_state=2)
 
 regr = LinearRegression()
-print("here okay 2")
-regr.fit(X_train,Y_train)
+
+regr.fit(X_train,y_train)
+pred = regr.predict(X_test)
+print(pred)
+print("accurace : ")
+print(regr.score(X_test,y_test))
+#git config --global --edit::
+
